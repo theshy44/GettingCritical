3c55472014ea06a61cee7889b06787e643289a81
theshy <theshy44@gmail.com>
2018-05-16 15:10:34 +0200

    First Commit

---
+++ task2.js
@@ -0,0 +1,13 @@
+var greenBox = document.getElementById('box1');
+var greenButton = document.getElementById('button1');
+
+greenButton.addEventListener('click', function(){
+greenBox.style.backgroundColor = 'green'
+})
+var redBox = document.getElementById('box2');
+var redButton = document.getElementById('button2');
+
+redButton.addEventListener('click', function(){
+redBox.style.backgroundColor = 'red'
+
+})
